f969a4b00b27018285f456120375171c56bb2584
HollowFalls <69210852+HollowFalls@users.noreply.github.com>
2022-04-19 12:45:01 +0200

    script

--- script.js
+++ script.js
@@ -4,7 +4,3 @@ let SubmitLabel = document.getElementById("Submitted");
 function onTextFocus() {
 	SubmitLabel.style.color = "#242423";
 }
-
-function changeColor() {
-	SubmitLabel.style.color = "red";
-}
